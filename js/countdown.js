85ff34f000d82af09a1d9c00de9a4e8cdbd49240
Wllew4 <soupsepicgamesbusiness@gmail.com>
2020-09-19 14:22:50 -0400

    updated extra life date

--- js/countdown.js
+++ js/countdown.js
@@ -1,4 +1,4 @@
-var cdTo = new  Date("Nov 7, 2020 8:00:00").getTime();
+var cdTo = new  Date("Nov 14, 2020 8:00:00").getTime();
 var x = setInterval(function() {
     var now = new Date().getTime();
     var distance = cdTo - now;
