cd76dc08e045651f7a9803ba22c80859f9cb1fb0
Wllew4 <willjanelle2@gmail.com>
2021-12-24 15:16:16 -0500

    switch to express

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,20 @@
-import * as https from 'http'
-import * as fs from 'fs'
-import * as path from 'path'
+import express from 'express'
+import path from 'path'
+import fs from 'fs'
 
+const PORT = process.env.PORT || 443
+const app = express()
+
+//	Let front handle misc paths
+app.get('*', (_, res) =>
+{
+	res.sendFile(path.join(__dirname, 'site/html/container.html'))
+})
+
+//	Serve static files
+app.use(express.static('build/site'))
+
+//	Redirect handling
 type Redirect =
 {
     url: string,
@@ -14,82 +27,16 @@ let redirects: Redirect[] = JSON.parse(
         { encoding: 'utf8' }
     ));
 
-function getContentType(file: string) : string
+for(let redirect of redirects)
 {
-    let extname     = path.extname(file);
-    let contentType = 'text/html';
-    switch (extname)
-    {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;      
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        case '.svg':
-            contentType = 'image/svg+xml';
-            break;
-        case '.GIF':
-            contentType = 'image/gif';
-            break;
-        case '.wav':
-            contentType = 'audio/wav';
-            break;
-        case '.txt':
-            contentType = 'text/plain';
-            break;
-    }
-    return contentType;
+	app.get('/' + redirect.url, (_, res) =>
+	{
+		res.redirect(302, redirect.redirect)
+	})
 }
 
-https.createServer(function(req, res)
+app.listen(PORT, () =>
 {
-    let filePath = './build/site' + req.url;
-    let contentType = getContentType(filePath);
-
-    for(let redirect of redirects)
-        if('/' + redirect.url == req.url)
-        {
-            res.writeHead(302, { 'Location': redirect.redirect });
-            res.end();
-        }
-    
-    fs.readFile(filePath, function(err, dat)
-    {
-        if(!err)
-        {
-            //  Return file if found
-            res.writeHead(200, {'Content-Type': contentType});
-            res.write(dat, 'utf-8');
-            res.end();
-        }
-        else
-        {
-            //  Let frontend handle non-file/unknown file requests
-            fs.readFile('build/site/html/container.html', function(err, dat)
-            {
-                if(!err)
-                {
-                    res.writeHead(200, {'Content-Type': contentType});
-                    res.write(dat, 'utf-8');
-                    res.end();
-                }
-                else
-                {
-                    console.log('Failed to load container.html!!' + err);
-                    res.end();
-                }
-            });
-        }
-    });
-    
-}).listen(process.env.PORT || 443);
+	console.log(`Listening on port ${PORT}`)
+	console.log(`Running at ${__dirname}`)
+})
