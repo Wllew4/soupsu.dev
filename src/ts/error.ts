336bdd0de90d0529df08a6b738b0bfd8846c96a1
Wllew4 <willjanelle2@gmail.com>
2021-12-30 21:56:44 -0500

    optimization

--- src/ts/error.ts
+++ src/ts/error.ts
@@ -1,5 +1,12 @@
-export function err404(code: number, message: string)
+import { createApp } from 'vue'
+import error from '../pages/error.html'
+
+export function loadError(code: number, message: string)
 {
-	$('#error-code').html('Error ' + code.toString());
-	$('#error-msg').html(message);
+	require('../scss/error.scss');
+	document.getElementsByTagName('body')[0].innerHTML = error;
+	createApp({ data() { return {
+		error_code: code,
+		error_msg: message
+	} } }).mount('#error');
 }
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,31 @@
-import './pageManager'
+import { loadHome } from './home'
+import { loadError } from './error'
+
+function updateBody ()
+{
+    switch(getUrl(window.location.pathname, 1))
+    {
+        case '':
+			loadHome();
+            break;
+        default:
+			loadError(404, 'Unable to locate<br>' + window.location.href);
+            break;
+    }
+}
+
+function getUrl(s: string, i: number)
+{
+    let t = s.split('/');
+    return t[i];
+}
+
+window.onpopstate = (e) =>
+{
+    updateBody();
+}
+
+window.onload = () =>
+{
+    updateBody();
+}
