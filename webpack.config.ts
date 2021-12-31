e77f9c6f99c9144db6448b62422af21d84984d8a
Wllew4 <willjanelle2@gmail.com>
2021-12-31 15:32:04 -0500

    potential build ram fix

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,77 +3,76 @@ import { CleanWebpackPlugin } from 'clean-webpack-plugin'
 import { VueLoaderPlugin } from 'vue-loader'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 
-module.exports = [
-	{
-		entry: './src/client/ts/main.ts',
-		devtool: 'source-map',
-		module: {
-			rules: [
-				{
-					test: /\.vue$/,
-					loader: 'vue-loader',
-					exclude: /node_modules/,
-				},
-				{
-					test: /\.ts$/,
-					loader: 'ts-loader',
-					exclude: /node_modules/,
-					options: {
-						appendTsSuffixTo: [/\.vue$/]
-					}
-				},
-				{
-					test: /\.scss$/,
-					use: [
-						"style-loader",
-						"css-loader",
-						"sass-loader"
-					],
-					exclude: /node_modules/,
-				},
-				{
-					test: /\.css$/,
-					use: [
-						"style-loader",
-						"css-loader"
-					],
-					exclude: /node_modules/,
-				},
-				{
-					test: /\.html$/,
-					use: [
-						{
-							loader: 'html-loader',
-							options: {
-								minimize: true
-							}
-						}
-					],
-					exclude: /node_modules/,
-				},
-				{
-					test: /(\.svg)|(felix[1-5].webp)|(biflag.webp)$/,
-					loader: 'file-loader',
-					options: {
-						name: '[name].[ext]'
-					}
+module.exports = 
+{
+	entry: './src/client/ts/main.ts',
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.ts$/,
+				loader: 'ts-loader',
+				exclude: /node_modules/,
+				options: {
+					appendTsSuffixTo: [/\.vue$/]
 				}
-			]
-		},
-		resolve: {
-			extensions: ['.ts', '.js', '.vue'],
-		},
-		output: {
-			filename: 'bundle.js',
-			path: path.resolve(__dirname, 'bin/client'),
-		},
-		plugins: [
-			new CleanWebpackPlugin(),
-			new HtmlWebpackPlugin({
-				template: 'src/client/html/container.html',
-				minify: true
-			}),
-			new VueLoaderPlugin()
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					"style-loader",
+					"css-loader",
+					"sass-loader"
+				],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: [
+					"style-loader",
+					"css-loader"
+				],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: {
+							minimize: true
+						}
+					}
+				],
+				exclude: /node_modules/,
+			},
+			{
+				test: /(\.svg)|(felix[1-5].webp)|(biflag.webp)$/,
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]'
+				}
+			}
 		]
-	}
-];
+	},
+	resolve: {
+		extensions: ['.ts', '.js', '.vue'],
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'bin/client'),
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: 'src/client/html/container.html',
+			minify: true
+		}),
+		new VueLoaderPlugin()
+	]
+}
