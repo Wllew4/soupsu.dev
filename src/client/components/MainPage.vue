844e0268c3c702b05fc140949d29ceaba79a142c
Wllew4 <willjanelle2@gmail.com>
2022-01-04 15:44:17 -0500

    HUGE progress on tabs

--- src/client/components/MainPage.vue
+++ src/client/components/MainPage.vue
@@ -2,15 +2,17 @@
 	<div id="container" class="center-children">
 		<div id="main-block">
 			<div id="tabs" class="text">
-				<div @click="selectTab(0)" class="clickable tab">
+				<div @click="selectTab(0)" class="clickable tab" v-bind:class="{ activeTab: activeTab==0 }">
 					Home {{activeTab}}
 				</div>
-				<div @click="selectTab(1)" class="clickable tab">
+				<div @click="selectTab(1)" class="clickable tab" v-bind:class="{ activeTab: activeTab==1 }">
 					Projects
 				</div>
 			</div>
-			<Home v-if="activeTab == 0"/>
-			<Projects v-if="activeTab == 1"/>
+			<div id="content">
+				<Home v-if="activeTab == 0"/>
+				<Projects v-if="activeTab == 1"/>
+			</div>
 		</div>
 	</div>
 </template>
@@ -23,7 +25,11 @@
 	export default defineComponent({
 		data() {
 			return {
-				activeTab: 0
+				activeTab: 0,
+				tabs: [
+					Home,
+					Projects
+				]
 			}
 		},
 		methods: {
@@ -45,9 +51,12 @@
 	@use '../scss/mobile';
 	@use '../scss/spacebg';
 
+	$pad: 2.25rem;
+
 	body
 	{
 		padding: 5%;
+		overflow-x: hidden;
 	}
 
 	#container
@@ -61,22 +70,35 @@
 	{
 		max-width: 90%;
 		width: 45rem;
-		padding: 3%;
-		padding-top: 0;
-		border-radius: 30px;
-		background-color: rgba(36, 0, 51, 0.8);
 	}
 
 	#tabs
 	{
 		display: flex;
 		flex-direction: row;
-		height: 3%;
+		height: $pad;
+	}
+
+	#content
+	{
+		min-height: 25rem;
+		padding: $pad;
+		background-color: rgba(36, 0, 51, 0.8);
+		border-radius: 0 0 30px 30px;
 	}
 
 	.tab
 	{
 		flex-grow: 1;
-		background-color: aquamarine;
+		border-radius: 30px 30px 0 0;
+		background-color: rgba(36, 0, 51, 0.4);
+		text-align: center;
+		color: white;
+		font-family: 'Noto Sans JP';
+	}
+
+	.activeTab
+	{
+		background-color: rgba(36, 0, 51, 0.8);
 	}
 </style>
